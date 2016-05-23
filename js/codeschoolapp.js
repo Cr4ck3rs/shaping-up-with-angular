3c2071647bcabd5467a6f52f47f4a772a417d9c7
Jorge Alberto Agüero Herrera <jaguero@belatrix.com>
2016-05-23 17:05:54 -0500

    Added comments and alternative syntax

--- js/codeschoolapp.js
+++ js/codeschoolapp.js
@@ -1,10 +1,24 @@
 (function(){
+
+  // This is how to manually bootstrap the application to angular
+  // Don't us ng-app alongside this
+  angular.element(document).ready(function() {
+    angular.bootstrap(angular.element("body")[0], ['store'], {
+      strictDi: true
+    });
+  });
+
   var store = angular.module('store', ['store-products']);
 
+  // using the ['dependency', function(dependency)] is called inline injection.
+  // you could also use controller.$inject = [dependecies array].
+  // implicit injection(not anotating them) should be avoided as
+  // it can cause problems when minifying.
   store.controller('StoreController', ['$http', function($http){
     var vm = this;
     vm.products = [];
 
+    //$http is an angular built in service
     $http.get('json/products.json').success(function(data){
       vm.products = data;
     });
